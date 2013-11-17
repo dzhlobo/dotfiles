e9b2293d1d6aca15ce96d54ea33fe11fc189a9d9
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2013-11-18 01:48:18 +0300

    Add zsh config
    You should have oh-my-zsh installed.

---
+++ zsh/custom/plugins/git/git.plugin.zsh
@@ -0,0 +1,22 @@
+# Aliases
+alias g="git"
+alias gst="git status -s"
+alias gco="git checkout"
+alias gbr="git branch"
+
+alias gpom="git push origin master"
+alias gp="git push"
+alias gpo="git push origin"
+alias gphm="git push heroku master"
+
+alias ga="git add"
+alias gci="git commit"
+
+alias gl="git --no-pager log --graph --pretty=tformat:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset [%an]' --abbrev-commit --date=short -20"
+alias gla="git --no-pager log --graph --pretty=tformat:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset [%an]' --abbrev-commit --date=short -20 --all"
+
+alias gpr="git pull --rebase"
+alias gm="git merge"
+
+alias gd="git diff"
+alias gdc="git diff --cached"
