33213fedbc9398a7f2eb71ede15b9ad36f99b857
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2013-12-03 20:38:59 +0300

    Show creation date in log, not date of last commit

--- zsh/custom/plugins/git/git.plugin.zsh
+++ zsh/custom/plugins/git/git.plugin.zsh
@@ -1,4 +1,5 @@
 # Aliases
+
 alias g="git"
 alias gst="git status -s"
 alias gco="git checkout"
@@ -12,8 +13,8 @@ alias gphm="git push heroku master"
 alias ga="git add"
 alias gci="git commit"
 
-alias gl="git --no-pager log --graph --pretty=tformat:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset [%an]' --abbrev-commit --date=short -20"
-alias gla="git --no-pager log --graph --pretty=tformat:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset [%an]' --abbrev-commit --date=short -20 --all"
+alias gl="git --no-pager log --graph --pretty=tformat:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ar)%Creset [%an]' --abbrev-commit --date=short -20"
+alias gla="git --no-pager log --graph --pretty=tformat:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ar)%Creset [%an]' --abbrev-commit --date=short -20 --all"
 
 alias gpr="git pull --rebase"
 alias gm="git merge"
